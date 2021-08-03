f9c8ea3f0baf846f0a0e4de159724871dc176c36
Matthias Knopp <matthias-knopp@gmx.net>
2021-08-03 13:39:14 +0200

    chore: add conditional logic

--- fast-forward-merge.sh
+++ fast-forward-merge.sh
@@ -4,3 +4,18 @@ set -e
 
 echo "first argument: $1"
 echo "second argument: $2"
+
+echo "checking out $1"
+git checkout $1
+
+
+echo "checking if '$1' can be merged into '$2'"
+git merge-base --is-ancestor $2 $1
+
+if [[ $? -eq 0 ]]; then
+  echo "merging (ff) '$1' -> '$2'"
+  git merge $2
+else
+  echo "no ff (fast forward) merge is possible"
+  exit 1
+fi
