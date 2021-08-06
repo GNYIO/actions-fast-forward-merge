804e517a798ee56067ffb88242ec391a70f05260
Matthias Knopp <matthias-knopp@gmx.net>
2021-08-06 10:06:49 +0200

    fix: argument order

--- fast-forward-merge.sh
+++ fast-forward-merge.sh
@@ -7,8 +7,8 @@ echo "fetching all branches"
 git fetch --all
 echo ""
 
-echo "checking out $1"
-git checkout $1
+echo "checking out '$2'"
+git checkout $2
 echo ""
 
 echo "printing all local branches..."
@@ -16,11 +16,11 @@ git branch --list
 echo ""
 
 echo "checking if '$1' can be merged into '$2'"
-git merge-base --is-ancestor $1 $2
+git merge-base --is-ancestor $2 $1
 
 if [[ $? -eq 0 ]]; then
   echo "merging (ff) '$1' -> '$2'"
-  git merge $2
+  git merge $1
 else
   echo "no ff (fast forward) merge is possible"
   exit 1
