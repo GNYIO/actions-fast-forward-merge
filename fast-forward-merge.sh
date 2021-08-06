1582149324762d0019cd89bf5b485cc0a664194a
Matthias Knopp <matthias-knopp@gmx.net>
2021-08-06 10:01:57 +0200

    fix: fetch all branches

--- fast-forward-merge.sh
+++ fast-forward-merge.sh
@@ -3,11 +3,17 @@
 echo "first argument: $1"
 echo "second argument: $2"
 
+echo "fetching all branches"
+git fetch --all
+echo ""
+
 echo "checking out $1"
 git checkout $1
+echo ""
 
 echo "printing all local branches..."
 git branch --list
+echo ""
 
 echo "checking if '$1' can be merged into '$2'"
 git merge-base --is-ancestor $1 $2
